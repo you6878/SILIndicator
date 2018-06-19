98444387f45a3a8edbed9297fdaf2b1dd059bfa6
Myeongsic <you6878@icloud.com>
2018-06-19 22:55:27 +0900

    init

--- HowlIndicator/ViewController.swift
+++ HowlIndicator/ViewController.swift
@@ -7,9 +7,22 @@
 //
 
 import UIKit
+import NVActivityIndicatorView
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,NVActivityIndicatorViewable {
 
+    @IBAction func button(_ sender: Any) {
+        startAnimating(CGSize(width: 30, height: 30), message: "Loading..", type: NVActivityIndicatorType(rawValue: 1))
+        
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1.5) {
+            NVActivityIndicatorPresenter.sharedInstance.setMessage("Authenticating...")
+        }
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 3) {
+            self.stopAnimating()
+            self.performSegue(withIdentifier: "nextPage", sender: nil)
+        }
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
